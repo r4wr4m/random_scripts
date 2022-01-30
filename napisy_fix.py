6faaa9af5edb5173ca2cc1b6a7e3555cfecd9550
r4wr4m <61500192+r4wr4m@users.noreply.github.com>
2022-01-30 11:50:13 +0100

    Update napisy_fix.py

--- napisy_fix.py
+++ napisy_fix.py
@@ -9,12 +9,14 @@ for FileName in glob.glob("**/*.txt", recursive = True):
     newText=newText.replace('Ä…','ą')
     newText=newText.replace('Ä‡','ć')
     newText=newText.replace('Ä™','ę')
+    newText=newText.replace('Ä�','Ę')
     newText=newText.replace('Ĺ‚','ł')
     newText=newText.replace('Ĺ�','Ł')
     newText=newText.replace('Ĺ›','ś')
     newText=newText.replace('Ĺš','Ś')
     newText=newText.replace('Ĺ„','ń')
     newText=newText.replace('Ăł','ó')
+    newText=newText.replace('Ă“','Ó')
     newText=newText.replace('Ĺş','ź')
     newText=newText.replace('Ĺą','Ź')
     newText=newText.replace('ĹĽ','ż')
@@ -25,3 +27,5 @@ for FileName in glob.glob("**/*.txt", recursive = True):
 
     with open(FileName, "w",encoding='utf8') as f:
         f.write(newText)
+
+os.system('pause')
