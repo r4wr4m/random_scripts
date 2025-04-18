2d01a14666854d9f6f43e1048053bf4ccbd96998
r4wr4m <61500192+r4wr4m@users.noreply.github.com>
2025-04-18 14:43:38 +0200

    Add files via upload

---
+++ PresentationMode.bat
@@ -0,0 +1,5 @@
+powercfg /change standby-timeout-ac 0
+powercfg /change standby-timeout-dc 0
+
+powercfg /change monitor-timeout-ac 0
+powercfg /change monitor-timeout-dc 0
---
+++ StopPresentationMode.bat
@@ -0,0 +1,5 @@
+powercfg /change standby-timeout-ac 5
+powercfg /change standby-timeout-dc 3
+
+powercfg /change monitor-timeout-ac 5
+powercfg /change monitor-timeout-dc 3
