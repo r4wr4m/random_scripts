79711ca1010a16fb64355c5281543faf5d9277e4
r4wr4m <61500192+r4wr4m@users.noreply.github.com>
2025-05-06 08:18:09 +0200

    Add files via upload

---
+++ BurpON.bat
@@ -0,0 +1,3 @@
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d 127.0.0.1:8080 /f
+pause
