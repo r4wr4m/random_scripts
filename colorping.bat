09be32b14e3f42f5eb019f215acb87cc2b167507
r4wr4m <61500192+r4wr4m@users.noreply.github.com>
2025-05-06 10:46:13 +0200

    Update colorping.bat
    fixed success check
    passing target as argument

--- colorping.bat
+++ colorping.bat
@@ -1,11 +1,16 @@
 @echo off 
+REM Usage: colorping.bat [target]
+REM Pings 8.8.8.8 by default
 
 set SUCCESSES=0
 set FAILURES=0
+set ADDRESS=8.8.8.8
+
+IF NOT [%1] == [] set ADDRESS=%1
 
 REM PING
 :ping
-ping -n 1 8.8.8.8 > nul
+ping -n 1 %ADDRESS% | findstr "TTL"
 if %errorlevel% equ 0 goto success
 if %errorlevel% equ 1 goto failure
 
@@ -15,7 +20,6 @@ set /A SUCCESSES=SUCCESSES+1
 cls
 color 2f
 echo SUCCESS 
-echo 
 echo (success: %SUCCESSES%, failure: %FAILURES%)
 goto wait
 
@@ -25,6 +29,7 @@ set /A FAILURES=FAILURES+1
 cls
 color 4f
 echo FAILURE
+echo (success: %SUCCESSES%, failure: %FAILURES%)
 goto wait
 
 REM WAIT
